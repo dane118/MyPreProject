36f882fd0123fb68e498a7b62152333d3e3a279e
Dani <120296813+dane118@users.noreply.github.com>
2023-11-16 17:29:21 +0700

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Configuration configuration = new Configuration();
+
     }
 }
